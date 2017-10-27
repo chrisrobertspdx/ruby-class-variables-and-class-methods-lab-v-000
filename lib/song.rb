986434d981a54c88cf27286caffd121f1835ca1e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-27 21:03:55 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name,:artist,:genre
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count =+ 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.to_set.sort
+  end
+
+  def self.genres
+    @@genres.to_set.sort
+  end
+
+  def self.genre_count
+    genre_count_hash = {}
+    @@genres.to_set.sort.each{|e|
+      genre_count_hash[e] = @@genres.count(e)
+    }
+  end
+  
+
+end
